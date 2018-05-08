9c31b7a0ba946f328b37da5bd2a404d36d514537
Vladimír Macháček <8machy@seznam.cz>
2018-05-08 11:29:09 +0200

    Added method for testing if files are equal, fixed cs

--- tests/nette-tester/AbstractTestCase.php
+++ tests/nette-tester/AbstractTestCase.php
@@ -13,16 +13,26 @@ declare(strict_types = 1);
 
 namespace ComposerSynchronizer\Tests;
 
+use Tester\Assert;
 use Tester\TestCase;
 
 
 abstract class AbstractTestCase extends TestCase
 {
 
-	protected function fileContains(string $filePath, string $content): bool
+	protected final function fileContains(string $filePath, string $content): bool
 	{
 		$fileContent = file_get_contents($filePath);
 		return strpos($fileContent, $content) !== false;
+	}
+
+
+	protected final function matchFile($expectedFilePath, $actualFilePath): void
+	{
+		$expectedFileContent = file_get_contents($expectedFilePath);
+		$actualFileContent = file_get_contents($actualFilePath);
+		Assert::true(is_file($actualFilePath));
+		Assert::equal($expectedFileContent, $actualFileContent);
 	}
 
 }
