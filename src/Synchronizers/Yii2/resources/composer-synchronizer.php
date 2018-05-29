0051f66c6838309468d6840ce0d801897a6492d3
Vladimír Macháček <8machy@seznam.cz>
2018-05-29 21:26:52 +0200

    Added strict types declaration

--- src/Synchronizers/Yii2/resources/composer-synchronizer.php
+++ src/Synchronizers/Yii2/resources/composer-synchronizer.php
@@ -1,3 +1,5 @@
-// This file was created by composer-synchronizer/composer-synchronizer package
 <?php
+// This file was created by composer-synchronizer/composer-synchronizer package
+// Remove declare(strict_types = 1) if necessary
+declare(strict_types = 1);
 
