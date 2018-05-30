1a1095e2873c1792501aaa867df777c575a33d47
Vladimír Macháček <8machy@seznam.cz>
2018-05-30 19:19:06 +0200

    Fixed cs

--- src/Synchronizers/Yii2/resources/composer-synchronizer.php
+++ src/Synchronizers/Yii2/resources/composer-synchronizer.php
@@ -1,6 +1,5 @@
 <?php
-
-// This file was created by composer-synchronizer/composer-synchronizer package
+/* This file was created by composer-synchronizer/composer-synchronizer package */
 
 declare(strict_types = 1);
 
